30baf6f95924c7f86eaff74d34261c05b55e013e
LaredjYacine <laredjyacine955@gmail.com>
2026-08-30 19:59:27 +0100

    Feat : Cleaned code

--- src/fine_tune_llm/Agent.py
+++ src/fine_tune_llm/Agent.py
@@ -1,15 +1,20 @@
 from os import name
-
+import ast
 from openai import OpenAI
-import wikipediaapi
-
-async def Search(input:str):
-    wiki = wikipediaapi.AsyncWikipedia(user_agent='MyProjectName (merlin@example.com)', language='en')
-    page= wiki.page(f'{input}')
-    if await page.exists():
-        summary = await page.summary
-        return summary
+import wikipedia
+def Search(query:str):
+    page = wikipedia.search(query)
+    if  page:
+         return page
     return 'Page not found'
+
+
+
+def Search_page(query):
+  page = wikipedia.summary(query,auto_suggest=False)
+  if page :
+    return page
+  return None
 
 
 def Calculator(A:int | float,B:int | float, operator :str):
@@ -27,52 +32,26 @@ def Calculator(A:int | float,B:int | float, operator :str):
             return 'Invalid operator'
 
 
-
 client =OpenAI(
     base_url='http://localhost:11434/v1/',
     api_key='ollama'
 )
 
-completion = client.chat.completions.create(
-    model='qwen2.5:7b',
-    messages=[
-        {
-        'role':'system','content':'You are a helpful assistant you will use The Functions if you need up to date information '
-        },
-        {
-            'role':'user','content':'Who is the current president of algeria of 2026'        }
-    ],
-    functions=[
-        {
-            "name": "Search",
-            "description":"Search wikipedia for up to date information and current news or facts",
-            "parameters" :{
-                "type": "object",
-                "properties":{"query":{"type":"string"}},
-                "required": ["query"]
-            }
-        },
-        {
-            "name":"Calculator",
-            "description":"Calculator for simple calculations",
-            "parameters":{
-                "type":"object",
-                "properties":
-                    {
-                        'A':{"type":"number", "description":"The first number"},
-                        'B':{"type":"number", "description":"The secondnumber"}
-                        ,'operator':{
-                            'type':'string',
-                            'enum':["+",'-','/','*'],
-                            'description':"the math operations to perform ",
-                        }
-                    }
-
-            , "required":['A','B','operator']
-            }
-
-        }
-    ]
-
+answer = client.responses.create(
+    model="qwen2.5:7b",
+    input="Who is the current president of albania ?",
+    tools=[{"name":"Search", "type": "Search"},{"type":"Calculator"}]  # type: ignore
 )
-print(response.choices[0].message.content)
+
+if answer.output[0].arguments:
+  asn = answer.output[0].arguments
+  query = ast.literal_eval(asn)
+  query = query['query']
+  res = Search(query)
+  summary_strings=[]
+  for result in res :
+    print(result)
+    summary = Search_page(result)
+    if summary is not None:
+      summary_strings.append(f'query is : {result} and summary is {summary}')
+  summary_strings= ','.join(summary_strings)
